3b9faaf87f2b528c42ac520bc63b44db35eea057
Hadrien Patte <hadrien.patte@protonmail.com>
2019-02-21 12:42:56 +0100

    Feat: update privkey and cert location
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_file(host):
-    file = host.file('/etc/letsencrypt/live/example.com/fullchain.pem')
+    file = host.file('/etc/ssl/private/example.com/fullchain.pem')
     assert file.exists
     assert file.user == 'root'
     assert file.group == 'root'
@@ -15,7 +15,7 @@ def test_file(host):
 
 def test_certificate(host):
     output = host.check_output(
-        'openssl x509 -in /etc/letsencrypt/live/example.com/fullchain.pem' +
+        'openssl x509 -in /etc/ssl/private/example.com/fullchain.pem' +
         ' -text -noout')
     assert ('Subject: CN = example.com, emailAddress = admin@example.com'
             in output)
