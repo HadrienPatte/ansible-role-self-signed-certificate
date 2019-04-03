5171b77b5d0196321ead19fbcdfaacc04b6d51df
Hadrien Patte <hadrien.patte@protonmail.com>
2019-04-04 01:19:58 +0200

    Fix: correct multi domains csr generation
    Define Subject Alterative Names to handle multiple domains support
    
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,7 +7,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_file(host):
-    file = host.file('/etc/ssl/private/example.com/fullchain.pem')
+    file = host.file('/etc/ssl/private/a.example.com/fullchain.pem')
     assert file.exists
     assert file.user == 'root'
     assert file.group == 'root'
@@ -15,8 +15,9 @@ def test_file(host):
 
 def test_certificate(host):
     output = host.check_output(
-        'openssl x509 -in /etc/ssl/private/example.com/fullchain.pem' +
+        'openssl x509 -in /etc/ssl/private/a.example.com/fullchain.pem' +
         ' -text -noout')
-    assert ('Subject: CN = example.com, emailAddress = admin@example.com'
+    assert ('Subject: CN = a.example.com, emailAddress = admin@a.example.com'
             in output)
     assert 'Public-Key: (4096 bit)' in output
+    assert 'DNS:a.example.com, DNS:b.example.com' in output
